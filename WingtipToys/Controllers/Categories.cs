31eb24392009b089603e0271474d6c2b067403bb
singhbikram <arcforyou@gmail.com>
2016-10-07 11:15:08 -0700

    First commit

---
+++ WingtipToys/Controllers/Categories.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WingtipToys.Models;
+
+namespace WingtipToys.Controllers
+{
+    public class Categories : Controller
+    {
+        //B:
+        private ProductContext  context = new ProductContext();
+        // GET: Categories
+        //public ActionResult Index() // Original line
+        public ViewResult Index()        
+        {
+            return View(context.Categories.ToList());
+        }
+    }
+}
